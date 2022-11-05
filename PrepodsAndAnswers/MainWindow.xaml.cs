66b856a5d82ad5f2ee5bc792cbf411e08bd02979
Ratfarn <farart98@gmail.com>
2022-11-05 16:03:27 +0300

    Добавьте файлы проекта.

---
+++ PrepodsAndAnswers/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrepodsAndAnswers
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var currentDiscipline = StudyEntities.GetContext().Disciplines.ToList();
+            datagrid.ItemsSource = currentDiscipline;
+        }
+
+        private void ButtonQA_Click(object sender, RoutedEventArgs e)
+        {
+            Disciplines discipline = (sender as Button).DataContext as Disciplines;
+            List <Questions> Q = discipline.Questions.ToList();
+            List<Answers> A = discipline.Answers.ToList();
+            MessageBox.Show($"Вопрос: {Q[0].Question}\nОтвет: {A[0].Correct}", "Вопросики на ответики");
+        }
+    }
+}
